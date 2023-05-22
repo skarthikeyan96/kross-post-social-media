3a1b68e12561f332e0395dd6d33530d12bebb1d9
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-22 19:27:46 +0530

    fix: get pull request contents

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
 import * as core from '@actions/core'
+import { Octokit } from '@octokit/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    // check if the "ready to publish" tag is present
-    // if the tag is not present do not run the action
-    // if the tag is present start the action
+    // get the pull request details
+    const github_token = core.getInput('GITHUB_TOKEN')
+    
+    const octokit = new Octokit({
+      auth: github_token
+    })  
+    
+    const {repo, owner, number} = github.context.issue
+    try {
+      const response = await octokit.request(
+        'GET /repos/{owner}/{repo}/pulls/{pull_number}',
+        {
+          owner,
+          repo,
+          pull_number: number,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        }
+      )
+
+      console.log(response)
+      }
+      catch(err) {
+        if (err instanceof Error) core.setFailed(err.message)
+
+      }
+    // get the markdown files 
+    // based on the api key provided make the social media request
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
