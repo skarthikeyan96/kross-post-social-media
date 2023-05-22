12e3d360d5aedd1ca2961477f88af3ffc02285ad
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-22 19:29:47 +0530

    fix: add the dist and update the actions

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import { Octokit } from '@octokit/core'
+import {Octokit} from '@octokit/core'
 import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     // get the pull request details
     const github_token = core.getInput('GITHUB_TOKEN')
-    
+
     const octokit = new Octokit({
       auth: github_token
-    })  
-    
+    })
+
     const {repo, owner, number} = github.context.issue
     try {
       const response = await octokit.request(
@@ -26,12 +26,10 @@ async function run(): Promise<void> {
       )
 
       console.log(response)
-      }
-      catch(err) {
-        if (err instanceof Error) core.setFailed(err.message)
-
-      }
-    // get the markdown files 
+    } catch (err) {
+      if (err instanceof Error) core.setFailed(err.message)
+    }
+    // get the markdown files
     // based on the api key provided make the social media request
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
