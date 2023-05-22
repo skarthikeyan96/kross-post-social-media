dab5cb2f51a1f9e9f8018d94672b493def1b3c31
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-22 18:44:23 +0530

    fix: add the dist content

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-   // check if the "ready to publish" tag is present 
-
-   // if the tag is not present do not run the action
-
-   // if the tag is present start the action
-
-  
+    // check if the "ready to publish" tag is present
+    // if the tag is not present do not run the action
+    // if the tag is present start the action
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
